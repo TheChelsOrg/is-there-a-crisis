945ed90ead6f95303c2a8475114653e7c0a6a4a0
MatBenfield <MatBenfield@users.noreply.github.com>
2020-09-01 21:19:12 +0100

    pylinting
    tidy up and errors only

--- python/build.py
+++ python/build.py
@@ -1,16 +1,18 @@
+"""
+Build and Tweet
+"""
  # importing modules
-import tweepy
+import re
+import os
 import csv
 import time
 import random
 import pathlib
-import re
-import os
-
+import tweepy
 # setup
 root = pathlib.Path(__file__).parent.parent.resolve()
 
-# secrets 
+# secrets
 consumer_key = os.getenv('c_key')
 consumer_secret = os.getenv('c_secret')
 access_token = os.getenv('a_token')
@@ -18,23 +20,23 @@ access_token_secret = os.getenv('a_secret')
 
 # functions
 def replace_chunk(content, marker, chunk):
-    r = re.compile(
+    replacer = re.compile(
         r"<!\-\- {} starts \-\->.*<!\-\- {} ends \-\->".format(marker, marker),
         re.DOTALL,
     )
     chunk = "<!-- {} starts -->\n{}\n<!-- {} ends -->".format(marker, chunk, marker)
-    return r.sub(chunk, content)
+    return replacer.sub(chunk, content)
 
 # authentication
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret) 
-auth.set_access_token(access_token, access_token_secret) 
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 # processing
 with open('data.csv') as csvfile:
- reader = csv.DictReader(csvfile)
- random_row = random.choice(list(reader))
- data_item_text = random_row['text']
+    reader = csv.DictReader(csvfile)
+    random_row = random.choice(list(reader))
+    data_item_text = random_row['text']
 
 if __name__ == "__main__":
     readme = root / "README.md"
@@ -43,5 +45,4 @@ if __name__ == "__main__":
     readme.open("w").write(rewritten)
 
     print (data_item_text)
-
-    api.update_status(status = "#CrisisChelsea Today's crisis at Chelsea is " + data_item_text)     
+    api.update_status(status = "#CrisisChelsea Today's crisis at Chelsea is " + data_item_text)
